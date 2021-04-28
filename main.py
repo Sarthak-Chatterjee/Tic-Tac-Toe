a3fecd3e9ba7bd8c161985cf4ff3775c120838e8
Sarthak Chatterjee <sarthakchat2000@gmail.com>
2021-04-29 01:50:31 +0530

    small fixes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Node:
 class Board:
 
     def __init__(self):
-        self.table = [[' ']*3 for i in range(3)]
+        self.table = [[' '] * 3 for _ in range(3)]
         self.available = {(col, row): True for col in range(3) for row in range(3)}
         self.winner = ''
 
@@ -22,7 +22,7 @@ class Board:
         self.winner = ''
 
     def __str__(self):
-        return '\n'+'\n---------\n'.join(' | '.join(col) for col in self.table)+'\n'
+        return '\n' + '\n---------\n'.join(' | '.join(col) for col in self.table) + '\n'
 
     def finished(self):
         for col in range(3):
@@ -48,7 +48,7 @@ class Player:
         self.sign = sign
 
     def next_move(self):
-        return divmod(int(input("enter a valid box number as shown above: "))-1, 3)
+        return divmod(int(input("enter a valid box number as shown above: ")) - 1, 3)
 
     def play(self, board):
         while True:
@@ -69,7 +69,8 @@ class Bot(Player):
         self.solution = Node()
 
     def play(self, board):
-        if not len(self.solution.next): self.solve(board)
+        if not len(self.solution.next):
+            self.solve(board)
         else:
             for move in self.solution.next:
                 if not board.available[move.val]:
@@ -80,27 +81,32 @@ class Bot(Player):
             if board.available[move.val] and self.solution.next[move] > val:
                 best_move = move
                 val = self.solution.next[move]
-        print("Opponent plays:", best_move.val[0]*3+best_move.val[1]+1)
+        print("Opponent plays:", best_move.val[0] * 3 + best_move.val[1] + 1)
         self.solution = best_move
         board.put(*best_move.val, self.sign)
 
     def let(self, board, move, sign):
-        wins = [-2,2][sign == self.sign]
+        wins = [-2, 2][sign == self.sign]
         col, row = move.val
         board.put(col, row, sign)
         if board.finished():
-            if board.winner == self.sign: wins = 1
-            elif board.winner == '': wins = 0
-            else: wins = -1
+            if board.winner == self.sign:
+                wins = 1
+            elif board.winner == '':
+                wins = 0
+            else:
+                wins = -1
             board.delete(move.val)
             return wins
 
         for col, row in board.available:
-            if board.available[(col,row)]:
+            if board.available[(col, row)]:
                 new_move = Node((col, row))
                 move.next[new_move] = self.let(board, new_move, ('X', 'O')[sign == 'X'])
-                if sign == self.sign: wins = min(wins, move.next[new_move])
-                else: wins = max(wins, move.next[new_move])
+                if sign == self.sign:
+                    wins = min(wins, move.next[new_move])
+                else:
+                    wins = max(wins, move.next[new_move])
 
         board.delete(move.val)
         return wins
@@ -116,7 +122,7 @@ class Game:
 
     def __init__(self, board, player1, player2):
         print("boxes and their numbers:")
-        print("\n---------\n".join((' | '.join(str(row*3+col+1) for col in range(3)) for row in range(3))))
+        print("\n---------\n".join((' | '.join(str(row * 3 + col + 1) for col in range(3)) for row in range(3))))
         self.board = board
         self.player1 = player1
         self.player2 = player2
@@ -125,9 +131,12 @@ class Game:
         move = self.player1.play(self.board)
         print(self.board)
         if self.board.finished():
-            if self.board.winner == '': print("IT'S A DRAW!!!")
-            elif type(self.player2) == Bot: print(f"YOU {('LOST', 'WON')[self.board.winner == self.player1.sign]}")
-            else: print(f"PLAYER{(2, 1)[self.board.winner == self.player1.sign]} WINS")
+            if self.board.winner == '':
+                print("IT'S A DRAW!!!")
+            elif type(self.player2) == Bot:
+                print(f"YOU {('LOST', 'WON')[self.board.winner == self.player1.sign]}")
+            else:
+                print(f"PLAYER{(2, 1)[self.board.winner == self.player1.sign]} WINS")
             return
         self.player2.play(self.board)
         print(self.board)
@@ -135,8 +144,10 @@ class Game:
 
 while True:
     n = input("choose:    1. vs Computer    2. Two Players\nEnter the corresponding number: ")
-    if n == '1': p2 = Bot('O')
-    elif n == '2': p2 = Player('O')
+    if n == '1':
+        p2 = Bot('O')
+    elif n == '2':
+        p2 = Player('O')
     else:
         print("Wrong Input")
         continue
